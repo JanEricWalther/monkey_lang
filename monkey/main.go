81fe3d97bf5e30da6aabd3fd204849352b592803
Jan Walther <account@janericwalther.de>
2023-06-23 12:22:51 +0000

    end of chapter 1

--- monkey/main.go
+++ monkey/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"monkey/repl"
+	"os"
 	// "os"
 )
 
 func main() {
 	fmt.Println("Hello! This is the Monkey programming language!")
+	fmt.Println("Feel free to type in commands")
+	repl.Start(os.Stdin, os.Stdout)
 }

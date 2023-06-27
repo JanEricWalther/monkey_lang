9edf3e0d6a90de62f63b3a5d7892b5613a30df7c
Jan Walther <account@janericwalther.de>
2023-06-27 14:11:18 +0000

    evaluating expression

--- monkey/repl/repl.go
+++ monkey/repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"monkey/eval"
 	"monkey/lexer"
 	"monkey/parser"
 )
@@ -41,8 +42,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErros(out, parser.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := eval.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 

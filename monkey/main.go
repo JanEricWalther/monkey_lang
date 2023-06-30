659fbb5c3beed35c4dae5ef4d9d07673c6666238
Jan Walther <account@janericwalther.de>
2023-06-30 12:19:40 +0000

    added interpreting input from files

--- monkey/main.go
+++ monkey/main.go
@@ -2,12 +2,48 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"monkey/eval"
+	"monkey/lexer"
+	"monkey/object"
+	"monkey/parser"
 	"monkey/repl"
 	"os"
 )
 
 func main() {
+	if len(os.Args) > 1 {
+		runFile(os.Args[1])
+	} else {
+		startRepl()
+	}
+}
+
+func startRepl() {
 	fmt.Println("Hello! This is the Monkey programming language!")
 	fmt.Println("Feel free to type in commands")
 	repl.Start(os.Stdin, os.Stdout)
+}
+
+func runFile(filename string) {
+	fContent, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("Error opening file %s\n", filename)
+		os.Exit(1)
+	}
+	input := string(fContent)
+
+	lexer := lexer.New(input)
+	parser := parser.New(lexer)
+	program := parser.ParseProgram()
+
+	if len(parser.Errors()) > 0 {
+		// printParserErros(os.Stderr, parser.Errors())
+		return
+	}
+	evaluated := eval.Eval(program, object.NewEnv())
+	if evaluated != nil {
+		io.WriteString(os.Stdout, evaluated.Inspect())
+		io.WriteString(os.Stdout, "\n")
+	}
 }

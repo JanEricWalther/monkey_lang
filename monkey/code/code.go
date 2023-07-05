a40470cfe734012e6cca1308afbc6eedf8040418
Jan Walther <account@janericwalther.de>
2023-07-05 14:23:23 +0000

    compiler: added bytecode setup

---
+++ monkey/code/code.go
@@ -0,0 +1,104 @@
+package code
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+)
+
+type Instructions []byte
+
+func (in Instructions) String() string {
+	var out bytes.Buffer
+
+	for i := 0; i < len(in); i++ {
+		def, err := Lookup(in[i])
+		if err != nil {
+			fmt.Fprintf(&out, "ERROR: %s\n", err)
+			continue
+		}
+		operands, read := ReadOperands(def, in[i+1:])
+		fmt.Fprintf(&out, "%04d %s\n", i, in.fmtInstruction(def, operands))
+
+		i += read
+	}
+	return out.String()
+}
+
+func (in Instructions) fmtInstruction(def *Definition, operands []int) string {
+	opCount := len(def.OpWidths)
+	if len(operands) != opCount {
+		return fmt.Sprintf("ERROR: operand len %d does not match defined %d\n", len(operands), opCount)
+	}
+	switch opCount {
+	case 1:
+		return fmt.Sprintf("%s %d", def.Name, operands[0])
+	}
+	return fmt.Sprintf("ERROR: unhandled operandCount for %s\n", def.Name)
+}
+
+type Opcode byte
+
+type Definition struct {
+	Name     string
+	OpWidths []int
+}
+
+const (
+	OpConstant Opcode = iota
+)
+
+var definitions = map[Opcode]*Definition{
+	OpConstant: {"OpConstant", []int{2}},
+}
+
+func Lookup(op byte) (*Definition, error) {
+	def, ok := definitions[Opcode(op)]
+	if !ok {
+		return nil, fmt.Errorf("opcode %d undefined", op)
+	}
+	return def, nil
+}
+
+func Make(op Opcode, operands ...int) []byte {
+	def, ok := definitions[op]
+	if !ok {
+		return []byte{}
+	}
+
+	instructionLen := 1
+	for _, w := range def.OpWidths {
+		instructionLen += w
+	}
+
+	instruction := make([]byte, instructionLen)
+	instruction[0] = byte(op)
+
+	offset := 1
+	for i, o := range operands {
+		width := def.OpWidths[i]
+		switch width {
+		case 2:
+			binary.BigEndian.PutUint16(instruction[offset:], uint16(o))
+		}
+		offset += width
+	}
+	return instruction
+}
+
+func ReadOperands(def *Definition, ins Instructions) (operands []int, offset int) {
+	operands = make([]int, len(def.OpWidths))
+
+	for i, width := range def.OpWidths {
+		switch width {
+		case 2:
+			operands[i] = int(ReadUint16(ins[offset:]))
+		}
+		offset += width
+	}
+	return
+}
+
+func ReadUint16(ins Instructions) uint16 {
+	return binary.BigEndian.Uint16(ins)
+}
---
+++ monkey/code/code_test.go
@@ -0,0 +1,82 @@
+package code
+
+import "testing"
+
+func TestMake(t *testing.T) {
+	tests := []struct {
+		op       Opcode
+		operands []int
+		expected []byte
+	}{
+		{
+			OpConstant,
+			[]int{65534},
+			[]byte{byte(OpConstant), 255, 254},
+		},
+	}
+
+	for _, test := range tests {
+		instruction := Make(test.op, test.operands...)
+
+		if len(instruction) != len(test.expected) {
+			t.Errorf("instruction has wrong length. got %d, expected %d", len(instruction), len(test.expected))
+		}
+		for i, b := range test.expected {
+			if instruction[i] != test.expected[i] {
+				t.Errorf("wrong byte at position %d. got %d, expected %d", i, instruction[i], b)
+			}
+		}
+	}
+}
+
+func TestInstructionsString(t *testing.T) {
+	instructions := []Instructions{
+		Make(OpConstant, 1),
+		Make(OpConstant, 2),
+		Make(OpConstant, 65535),
+	}
+
+	expected := `0000 OpConstant 1
+0003 OpConstant 2
+0006 OpConstant 65535
+`
+
+	concatInst := Instructions{}
+	for _, ins := range instructions {
+		concatInst = append(concatInst, ins...)
+	}
+	if concatInst.String() != expected {
+		t.Errorf("instructions wrongly formatted.\nexpected %q\ngot %q", expected, concatInst.String())
+	}
+}
+
+func TestReadOperands(t *testing.T) {
+	tests := []struct {
+		op       Opcode
+		operands []int
+		byteRead int
+	}{
+		{
+			OpConstant,
+			[]int{65535},
+			2,
+		},
+	}
+
+	for _, test := range tests {
+		instruction := Make(test.op, test.operands...)
+		def, err := Lookup(byte(test.op))
+		if err != nil {
+			t.Fatalf("definition not found: %q", err)
+		}
+		operandsRead, n := ReadOperands(def, instruction[1:])
+		if n != test.byteRead {
+			t.Fatalf("wrong number of bytes expected %d, got %d", test.byteRead, n)
+		}
+		for i, expected := range test.operands {
+			if operandsRead[i] != expected {
+				t.Errorf("operand wrong. expected %d, got %d", expected, operandsRead[i])
+			}
+		}
+	}
+}
